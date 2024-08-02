8eee95946f4cef34c1202222a8e3cc1beb592e80
sperandio <ddosperandio@gmail.com>
2024-08-02 15:12:06 -0300

    Crie a 1ª estrutura do game no terminal

--- forca.py
+++ forca.py
@@ -1,10 +1,33 @@
 from estilo import limpaTela, l, msgAnimada
+from random import choice
 
 #Contantes
 TAM = 40
 
 #Título
 limpaTela()
-msgAnimada("-=" * (TAM // 2))
-msgAnimada(f"{'Jogo da Forca':^{TAM}}")
-msgAnimada("-=" * (TAM // 2))
+msgAnimada("-=" * (TAM // 2), False)
+msgAnimada(f"{'Jogo da Forca':^{TAM}}", False)
+msgAnimada("-=" * (TAM // 2), False)
+
+#Definindo as palavras do jogo
+palavras = ["arroz", "feijao", "batata", "uva", "morango"]
+
+#Desenhado a forca 
+print("""
+                +---+
+                |   |
+                O   |
+               /|\  |
+               / \  |
+                    |
+                ========
+""", end="\n")
+
+#Mostrando traços do tamanho da palavra
+for word in choice(palavras):
+    for letras in word:
+        print("_ ", end="")
+print("\n")
+
+l()
