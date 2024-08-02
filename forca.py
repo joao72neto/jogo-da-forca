222d00ca1154e2d89a218ec7032a08a40be77697
sperandio <ddosperandio@gmail.com>
2024-08-02 13:22:26 -0300

    Adicione um título para o jogo

--- forca.py
+++ forca.py
@@ -1,2 +1,10 @@
 from estilo import limpaTela, l, msgAnimada
 
+#Contantes
+TAM = 40
+
+#Título
+limpaTela()
+msgAnimada("-=" * (TAM // 2))
+msgAnimada(f"{'Jogo da Forca':^{TAM}}")
+msgAnimada("-=" * (TAM // 2))
