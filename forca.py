0449e6bb1df5d5fa40de78bf6f09a5cae48c8438
sperandio <ddosperandio@gmail.com>
2024-08-03 11:28:25 -0300

    Trate erros ao pegar as letras

--- forca.py
+++ forca.py
@@ -1,48 +1,59 @@
 from estilo import limpaTela, l, msgAnimada
 from random import choice
+from time import sleep
 
 #Contantes
 TAM = 40
 
+#Variáveis globais
+animacao = True
+
 #Título
-limpaTela()
-msgAnimada("——" * (TAM // 2))
-msgAnimada(f"{'Jogo da Forca':^{TAM}}")
-msgAnimada("——" * (TAM // 2))
-
-#Definindo as palavras do jogo
-palavras = ["arroz", "feijao", "batata", "uva", "morango"]
-
-# --------------------------------------------------------------------------- 
-
-#1ª Parte do desenho da forca
-msgAnimada("""
-        +---+
-        |   |
-        O   |
-       /|\  |     """, True, "", 0.018)
-
-
-#Mostranado os espaços das letras
-word = choice(palavras)
-msgAnimada("_ " * len(word), True, "", 0.018)
-
-
-#2ª perte do desenho da forca 
-msgAnimada("""
-       / \  |
-            |
-        ========
-""", True, "\n", 0.018)
-
-#Pedindo as letra para o usuário
-l("——")
 while True:
-    letra = input("Insira uma letra: ")
+    limpaTela()
+    msgAnimada("——" * (TAM // 2), animacao)
+    msgAnimada(f"{'Jogo da Forca':^{TAM}}", animacao)
+    msgAnimada("——" * (TAM // 2), animacao)
+
+    #Definindo as palavras do jogo
+    palavras = ["arroz", "feijao", "batata", "uva", "morango"]
+    word = palavras[0]
+    # --------------------------------------------------------------------------- 
+
+    #1ª Parte do desenho da forca
+    msgAnimada("""
+            +---+
+            |   |
+            O   |
+           /|\  |     """, animacao, "", 0.018)
+
+
+    #Mostranado os espaços das letras
+    msgAnimada("_ " * len(word), animacao, "", 0.018)
+
+
+    #2ª perte do desenho da forca 
+    msgAnimada("""
+           / \  |
+                |
+            ========
+    """, animacao, "\n", 0.018)
+
+    #Pedindo as letra para o usuário
+    l("——")
     
-    if letra[0].isnumeric():
-        print("")
+    letra = input("Insira uma letra: ")
+    try:
+        if letra[0].isnumeric():
+            animacao = False
+            continue
+        else:
+            word = choice(palavras)
+            animacao = True
+            break
+    except IndexError:
+        print("Espaço não é uma letra")
+        sleep(1)
+        animacao = False
         continue
-    else:
-        break
 l("——")
