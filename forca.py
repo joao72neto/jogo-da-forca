fb2ae3316814ebda13e2613f2e62ed90b369b778
sperandio <ddosperandio@gmail.com>
2024-08-20 21:36:48 -0300

    Faça os temas funcionarem

--- forca.py
+++ forca.py
@@ -1,5 +1,7 @@
 """Programa que simula um jogo da forca"""
 
+# pylint: disable=c0103, r0912
+
 # Imports Gerais
 from random import choice
 from estilo import limpaTela, l, msgAnimada, valorInvalido
@@ -108,7 +110,7 @@ def boneco(contador):
                 +---+
                 |   |
                 O   |
-               /|\  |     """, ANIMACAO, "", 0.018)
+               /|\\  |     """, ANIMACAO, "", 0.018)
 
         #Montando a palavra
         for i, v in enumerate(word):
@@ -131,7 +133,7 @@ def boneco(contador):
                 +---+
                 |   |
                 O   |
-               /|\  |     """, ANIMACAO, "", 0.018)
+               /|\\  |     """, ANIMACAO, "", 0.018)
 
 
         #Montando a palavra
@@ -171,7 +173,7 @@ def boneco(contador):
 
         #2ª perte do desenho da forca
         msgAnimada("""
-               / \  |
+               / \\  |
                     |
                 ========
         """, ANIMACAO, "\n", 0.018)
@@ -185,8 +187,18 @@ ANIMACAO = True
 TEMPO = 0.02
 
 # Definindo as palavras
-palavras = [
-    "amigo", "futuro", "coragem", "felicidade", "alegria", "esperança",
+palavras = {
+    "alimentos":
+    ["Pizza", "Pão", "Arroz", "Frango", "Sopa", "Bolo", "Queijo",
+    "Maçã", "Batata", "Peixe", "Manga", "Torta", "Uva", "Cereal",
+    "Milho", "Carne", "Salada", "Feijão", "Banana", "Melancia", "Ovo",
+    "Cenoura", "Morango", "Abacate", "Laranja", "Tomate", "Coco", "Melão",
+    "Biscoito", "Iogurte", "Macarrão", "Abobrinha", "Pimenta", "Pêra",
+    "Alface", "Berinjela", "Espinafre", "Azeitona", "Brócolis", "Castanha",
+    "Noz", "Amendoim", "Batata-doce", "Granola", "Açúcar", "Mel", "Goiaba",
+    "Lentilha", "Rabanete", "Couve"],
+    "sentimentos":
+    ["amigo", "futuro", "coragem", "felicidade", "alegria", "esperança",
     "amizade", "sabedoria", "liberdade", "saudade", "aventura", "carinho",
     "honestidade", "solidariedade", "justiça", "confiança", "RESPeito",
     "gentileza", "humildade", "empatia", "persistência", "gratidao",
@@ -197,33 +209,66 @@ palavras = [
     "compromisso", "serenidade", "honra", "dignidade", "coragem", "valentia",
     "liderança", "vigilância", "disciplina", "versatilidade", "sabedoria",
     "imparcialidade", "decisão", "criatividade", "dedicação", "zelo",
-    "firmeza", "confiança", "RESPeito", "sabedoria", "amizade", "esperança"
-]
+    "firmeza", "confiança", "Respeito", "sabedoria", "amizade", "esperança"],
+    "games":
+    ["Super Mario", "The Legend of Zelda", "Minecraft", "Fortnite", "Call of Duty",
+    "Overwatch", "The Witcher", "Red Dead Redemption", "Grand Theft Auto", "Halo",
+    "Assassin's Creed", "FIFA", "League of Legends", "Valorant","Cyberpunk 2077",
+    "Dark Souls", "Elden Ring", "Animal Crossing", "Pokémon", "Metroid", "Tetris",
+    "Street Fighter", "Mortal Kombat", "Tekken", "Resident Evil", "Silent Hill",
+    "Bloodborne", "Horizon Zero Dawn", "God of War", "Uncharted", "Tomb Raider",
+    "Final Fantasy", "Dragon Quest", "World of Warcraft", "Starcraft", "Diablo",
+    "The Sims", "SimCity", "Fallout", "Mass Effect", "Half-Life", "Dota 2",
+    "Counter-Strike", "Portal", "Apex Legends", "Rocket League", "PUBG", "Among Us",
+    "Celeste", "Stardew Valley", "Terraria", "Hades", "Undertale", "Bioshock",
+    "Gears of War", "Borderlands", "Far Cry", "Splatoon", "Destiny"],
+    "instrumentos":
+    ["Guitarra", "Piano", "Violino", "Bateria", "Baixo", "Flauta", "Trompete",
+    "Saxofone", "Clarinete", "Violoncelo", "Harpa", "Oboé", "Trombone", "Acordeão",
+    "Banjo", "Bandolim", "Ukulele", "Sintetizador", "Xilofone", "Harmônica", "Gaita de fole",
+    "Tímpano", "Marimba", "Conga", "Bongo", "Pandeiro", "Djembe", "Cajón",
+    "Flauta doce", "Órgão", "Fagote", "Piccolo", "Lira", "Triângulo", "Metalofone",
+    "Pratos", "Sitar", "Alaúde", "Rabeca", "Didgeridoo", "Flautas de pã", "Vibrafone",
+    "Clavicórdio", "Melódica", "Cítara", "Balalaica", "Kalimba", "Koto", "Tambura",
+    "Viola", "Maracas", "Tuba", "Eufônio", "Caixa", "Sinos", "Bumbo",
+    "Steel drum", "Handpan", "Ocarina"],
+    "filmes":
+    ["Titanic", "A Origem", "Avatar", "O Poderoso Chefão", "Pulp Fiction", "O Cavaleiro das Trevas",
+    "Forrest Gump", "Clube da Luta", "Matrix", "Gladiador", "Jurassic Park",
+    "Um Sonho de Liberdade", "Star Wars", "O Senhor dos Anéis", "Interestelar", "Os Vingadores",
+    "De Volta para o Futuro", "O Rei Leão", "A Lista de Schindler", "O Silêncio dos Inocentes",
+    "O Resgate do Soldado Ryan", "Alien", "O Exterminador do Futuro", "Coração Valente",
+    "Indiana Jones", "Os Bons Companheiros", "Casablanca", "Os Suspeitos", "Tubarão", "Rocky",
+    "E.T. - O Extraterrestre", "O Iluminado", "Scarface", "O Sexto Sentido",
+    "Seven: Os Sete Crimes Capitais", "Coringa", "Piratas do Caribe", "Vingadores: Ultimato",
+    "Harry Potter", "Capitão América", "Pantera Negra", "O Homem de Ferro", "O Hobbit",
+    "Guardiões da Galáxia", "O Lobo de Wall Street", "Django Livre", "O Grande Gatsby",
+    "A Origem dos Guardiões", "O Código Da Vinci", "Missão Impossível", "Os Incríveis",
+    "Homem-Aranha", "Thor: Ragnarok", "Jurassic World", "Os Oito Odiados", "A Bela e a Fera"]
+
+}
 
 
-# opções de TEMAS
-TEMASPossiveis = ["Comida", "Sentimentos", "Games", "Instrumentos", "Filmes"]
+# Temas disponíveis
+temasPossiveis = ["alimentos", "sentimentos", "games", "instrumentos", "filmes"]
 
-# Mostranso menus
+# Mostrando menus
 TEMAS = MAINTITLE = True
 
-while True:
-    # Escolhendo uma nova palavra
-    word = choice(palavras)
 
-    # Espaço para preencher as palavras
-    frase = ["_ "] * len(word)
+while True:
+
+    # Mostrar erros
     letrasUsadas = []
 
     # Resetando as letras
     LETRA = ""
     C = PRIMEIROERRO = 0
 
-    while True:
+    # Gerando a frase completa
+    frase = []
 
-        # Definindo as palavras do jogo
-        if frase == word:
-            word = choice(palavras)
+    while True:
 
         # ---------------------------------------------------------------------------
 
@@ -246,7 +291,7 @@ while True:
                     l("——")
 
                 # Escolhendo as categorias
-                msgAnimada("1 - Comida", ANIMACAO)
+                msgAnimada("1 - Alimentos", ANIMACAO)
                 msgAnimada("2 - Sentimentos", ANIMACAO)
                 msgAnimada("3 - Games", ANIMACAO)
                 msgAnimada("4 - Instrumentos", ANIMACAO)
@@ -275,6 +320,13 @@ while True:
                 ANIMACAO = True
                 break
 
+            # Escolhendo a palavra de acordo com o tema
+            word = choice(palavras[temasPossiveis[(TEMA-1)]]).lower()
+
+            # Espaço para preencher as palavras
+            for ele in word.split():
+                frase += ["_ "] * len(ele)
+                frase += " "
         # ---------------------------------------------------------------------------
 
         # Título para mostrar letras inválidas
@@ -299,7 +351,7 @@ while True:
 
         # Mostrando o tema selecionado
         l("——", TAM)
-        TEMAESCOLHIDO = "Tema Escolhido: " + "".join(TEMASPossiveis[TEMA-1])
+        TEMAESCOLHIDO = "Tema Escolhido: " + "".join(temasPossiveis[TEMA-1])
         msgAnimada(f"{TEMAESCOLHIDO:^{TAM*2}}", ANIMACAO)
 
         # 1ª Mostrando o boneco
@@ -352,7 +404,7 @@ while True:
     if C == 6:
         msgAnimada(f"{'Você morreu!':^{TAM}}", ANIMACAO)
         l("——")
-        print(f"A palavra era: {word}")
+        msgAnimada(f"A palavra era: {word}")
         l("——")
 
     else:
